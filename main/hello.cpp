e3c4cac1caf062c492a07937ac9afc597a33bb49
Jayant Anand <129861458+jay-and-main@users.noreply.github.com>
2025-03-13 15:34:55 +0530

    made changes for webhooks

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this trying and testing this now. My SRN is PES1UG22CS252. \n";
+  cout << "I have successfully built and run this trying and testing this now. My SRN is PES1UG22CS252. Trying to use webhooks \n";
   return 0;
 }
